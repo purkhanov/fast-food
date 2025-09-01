8026081477bbd72734330f82a1e715903371f460
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-09-01 23:50:13 +0500

    created onMounted logic

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,14 @@
 <script setup>
 import ProductMenu from './components/ProductMenu.vue'
 import Button from './components/OrderButton.vue'
+import { onMounted } from 'vue';
+import { useProductStore } from './stores/product';
+
+const productStore = useProductStore()
+
+// onMounted(async () => {
+//   await productStore.fetchProducts()
+// })
 </script>
 
 <style scoped>
