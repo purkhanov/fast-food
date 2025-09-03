e7f3c6690c26c66b0ceada035e0979df3b230605
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-09-03 14:36:43 +0500

    updated App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,79 @@
 <template>
-  <RouterView />
+  <Products class="product" :class="{'show-product': showOrder}" />
+  <Orders
+    @edit-clicked="productOrderToggle" 
+    class="order" 
+    :class="{'show-order': showOrder}" 
+  />
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from 'vue'
-import { tg } from './telegram-api'
+import { computed, ref, watch } from 'vue'
+import { useProductStore } from '@/stores/product'
+import { tg } from '@/telegram-obj'
+import Products from '@/components/Products.vue'
+import Orders from '@/components/Orders.vue'
 
+const productStore = useProductStore()
+const showOrder = ref(false)
+const btnText = "ЗАКАЗАТЬ"
 
+const total = computed(() => productStore.basket.reduce((acc, i) => acc + i.price * i.count, 0))
 
-onMounted(async () => {
-  console.log(tg.initData, "---")
+console.log(tg.colorScheme, "color scheme")
+
+tg.MainButton.hasShineEffect = true
+tg.MainButton.position = "bottom"
+tg.MainButton.setText(btnText)
+
+tg.MainButton.onClick(() => {
+  if (!showOrder.value) {
+    productOrderToggle()
+  } else {
+    console.log('Paying')
+  }
 })
+
+tg.BackButton.onClick(() => {
+  productOrderToggle()
+})
+
+watch(() => productStore.basket.length, (newLength) => {
+  if (newLength > 0) {
+    tg.MainButton.show()
+  } else {
+    tg.MainButton.hide()
+
+    if (showOrder.value) {
+      showOrder.value = false
+      tg.BackButton.hide()
+    }
+  }
+})
+
+function productOrderToggle() {
+  if (!showOrder.value) {
+    showOrder.value = true
+    tg.BackButton.show()
+    tg.MainButton.setText(`ОПЛАТИТЬ ${total.value}`)
+  } else {
+    showOrder.value = false
+    tg.MainButton.setText(btnText)
+    tg.BackButton.hide()
+  }
+}
 </script>
+
+<style scoped>
+.order {
+  display: none;
+}
+
+.show-order {
+  display: block;
+}
+
+.show-product {
+  display: none;
+}
+</style>
