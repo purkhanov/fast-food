29c2856334739c69b42ac02b29700facc937d085
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-09-03 12:54:43 +0500

    created Orders.vue

---
+++ frontend/src/components/Orders.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="order-overview">
+    <div class="order-block">
+      <div class="order-header-wrap">
+        <h2 class="order-header">Your Order</h2>
+        <span @click="edit" class="order-edit">Edit</span>
+      </div>
+
+      <OrderItem 
+        v-for="item in productStore.basket" 
+        :key="item.id" 
+        :product="item"
+      />
+
+      <div class="text-field-wrap">
+        <textarea 
+          class="text-field" 
+          rows="1" 
+          placeholder="Add comment…" 
+          style=""
+        ></textarea>
+        <div class="text-field-hint">
+          Any special requests, details, final wishes etc.
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import '@/assets/css/order-view.css'
+import { useProductStore } from '@/stores/product'
+import OrderItem from '@/components/OrderItem.vue'
+
+const productStore = useProductStore()
+</script>
