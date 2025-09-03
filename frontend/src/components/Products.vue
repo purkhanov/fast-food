b91d7ef098497806f1883ceac5944f5040b99697
Nurlan Purkhanov <ppurkhanov@gmail.com>
2025-09-03 12:54:30 +0500

    created Products.vue

---
+++ frontend/src/components/Products.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <ProductItem 
+      v-for="product in productStore.products" 
+      :key="product.id"
+      :product="product" 
+    />
+  </section>
+</template>
+
+<script setup>
+import { useProductStore } from '@/stores/product'
+import ProductItem from '@/components/ProductItem.vue'
+
+const productStore = useProductStore()
+
+</script>
+
+<style scoped>
+section {
+  max-height: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+  margin: 0 auto;
+  max-width: 480px;
+  transition: max-height var(--page-animation), opacity var(--page-animation);
+  background-color: var(--block-bg-color);
+}
+</style>
